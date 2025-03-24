b9f56c14ff7960f2558af4d8c5425099da1d70e9
Victor Chang <vicchang@nvidia.com>
2025-03-24 11:27:41 -0700

    Change package version

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -24,6 +24,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0")]
